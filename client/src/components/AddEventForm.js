c151f89c0ea726632bfee945d17b8dd31f2c9886
Zarina Gumbatova <26606767+Zarina2311@users.noreply.github.com>
2020-07-03 21:17:36 -0600

    center input label

--- client/src/components/AddEventForm.js
+++ client/src/components/AddEventForm.js
@@ -7,30 +7,27 @@ function AddEventForm() {
     const newEvent = new URLSearchParams({
       name: event.target.name.value, // "value" is user's input
       date: event.target.date.value,
-      category: event.target.category.value
+      category: event.target.category.value,
     });
 
     fetch("http://localhost:3000/events", {
       headers: {
-        Accept: "application/json"
+        Accept: "application/json",
       },
       body: newEvent,
-      method: "POST"
+      method: "POST",
     });
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <h4>Add Event</h4>
-      <label>Event name:</label>
+      <label className="input-label">Event name</label>
       <input type="text" name="name" />
-      <br />
-      <label>Event date:</label>
+      <label className="input-label">Event date</label>
       <input type="text" name="date" />
-      <br />
-      <label>Event category:</label>
+      <label className="input-label">Event category</label>
       <input type="text" name="category" />
-      <br />
       <input type="submit" />
     </form>
   );
